3164c0da4c62504faf91f29c879e1254d43fad16
xton3749 <xton3749@uni.sydney.edu.au>
2019-08-17 15:10:31 +1000

    add decoding

--- BCGDV.py
+++ BCGDV.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import base64
 
 
 def getKey():
@@ -11,20 +12,21 @@ def getKey():
     print(response.status_code)
     return apiKey
 
-
 def postKey(apiKey):
     url = "https://interns.bcgdvsydney.com/api/v1/submit?apiKey={}".format(apiKey)
 
+    name = base64.b64decode(b'WGl5YW4gVG9uZw==').decode("utf-8")
+    email = base64.b64decode(b'eGl5YW4udG9uZ0BvdXRsb29rLmNvbQ==').decode("utf-8")
+
     data = json.dumps({
-        "name": "Xiyan Tong",
-        "email": "xiyan.tong@outlook.com"	
+        "name": name,
+        "email": email	
     })
 
     response = requests.post(url, data=data)
 
     print(response.status_code) 
     print(response.text)    
-
 
 if __name__ == "__main__":
     postKey(getKey())
